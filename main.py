19b0b43a7fe8c2031425abe8b12a17dd173f76a6
ronaldlondono <ronaldlondono20@gmail.com>
2023-03-19 20:45:11 -0500

    codigo actualizado

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
-from fastapi import FastAPI
+from typing import Union
+from fastapi import FastAPI, Path, Query
 
 app = FastAPI()
 
 
-@app.get('/')
-def ruta_inicial():
-    return {"Bienvenido": "bienvenido a mi api"}
+# GET
+# POST
+# PUT
+# DELETE
+# PATCH
+# HEAD
+# OPTIONS
 
+lista_items = {1: 'Item 1', 2: 'Item 2', 3: 'Item 3'}
 
-@app.get('/prueba2')
-def prueba2():
-    return {"Bienvenido": "bienvenido a mi api"}
+# METODO PARA MOSTRAR UN ITEM ESPECIFICO
+@app.get("/items/{id_item}")
+async def show_item_name(id_item: int):
+    return lista_items[id_item]
+
+# METODO PARA MOSTRAR TODOS LOS ITEMS
+@app.get("/items")
+async def show_item_name():
+    return lista_items
+
+# METODO PARA AGREGAR
+@app.post("/items")
+async def add_item(value: str):
+    cantidad = len(lista_items) + 1
+    lista_items[cantidad] = value;
+    return "Se agrego el item al diccionario"
+
+# METODO PARA ACTUALIZAR
+@app.put("/items/{id_item}")
+async def update_item(id_item: int, new_value: str):
+    lista_items[id_item] = new_value
+    return "Valor Actualizado"
+
+# METODO PARA ELIMIAR UN ITEM
+@app.delete("/items/{id_item}")
+async def delete_item(id_item: int):
+    del lista_items[id_item]
+    return "El item ha sido eliminado"
