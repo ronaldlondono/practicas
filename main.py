863cf6e3fa3390c5b81edc32c8d5a496aa869405
leonardo ramires vasquez <duvanramirezvasquez@gmail.com>
2023-03-19 19:34:03 -0500

    agregando nueva ruta prueba 2

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get('/')
 def ruta_inicial():
-    return{"Bienvenido": "bienvenido a mi api"}
+    return {"Bienvenido": "bienvenido a mi api"}
+
+
+@app.get('/prueba2')
+def prueba2():
+    return {"Bienvenido": "bienvenido a mi api"}
